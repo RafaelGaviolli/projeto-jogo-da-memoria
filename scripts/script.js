822ec5315fb807ad3a659fce37d2b8ea09d8d225
Rafael Gaviolli <bygamerrcg@hotmail.com>
2021-03-26 22:37:29 -0300

    f2

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,29 @@ let techs = [
     'react',
 ];
 
+let cards = null;
+startGame();
+
+function startGame(){
+    let cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    console.log(cards)
+}
+
+function shuffleCards(cards){
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0){
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+
+    }
+}
+
 createCardsFromTechs(techs);
 function createCardsFromTechs(techs){
 
