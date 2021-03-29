942b0111b8d35909472ac57ad37c368b0c189e3b
Rafael Gaviolli <bygamerrcg@hotmail.com>
2021-03-29 20:30:17 -0300

    f3

--- scripts/script.js
+++ scripts/script.js
@@ -3,33 +3,17 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react',
-];
 
-let cards = null;
 startGame();
 
 function startGame(){
-    let cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-    
-    initializeCards(cards);
+    initializeCards(game.createCardsFromTechs());
 }
 
 function initializeCards(cards){
     let gameBoard = document.getElementById("gameBoard");
 
-    cards.forEach(card=>{
+    game.cards.forEach(card=>{
 
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
@@ -45,7 +29,6 @@ function initializeCards(cards){
 
     })
     
-
 }
 
 function createCardContent(card, cardElement){
@@ -70,48 +53,7 @@ function createCardFace(face, card, element){
 }
 
 
-function shuffleCards(cards){
-    let currentIndex = cards.length;
-    let randomIndex = 0;
 
-    while(currentIndex !== 0){
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
-
-    }
-}
-
-createCardsFromTechs(techs);
-function createCardsFromTechs(techs){
-
-    let cards = [];
-
-    techs.forEach((tech) => {
-        cards.push(createPairFromTech(tech));
-    })
-
-    return cards.flatMap(pair => pair);
-}
-
-function createPairFromTech(tech){
-
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() *1000);
-}
 
 function flipCard(){
 
